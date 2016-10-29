192a458de4cb6c1c93524e1bc0bbad807dce1d42
JaktensTid <jaktenstid1@gmail.com>
2016-10-29 17:03:16 +0000

    Исправлено: cronjob

--- TestCron.py
+++ TestCron.py
@@ -2,15 +2,15 @@ import datetime
 
 def test():
     try:
-        fh = open("Temporary/cronlog.txt", 'r')
+        fh = open("/home/Cogcc/CogccDatabase/Temporary/cronlog.txt", 'r')
         fh.close()
     except:
         # if file does not exist, create it
-        fh = open("Temporary/cronlog.txt", 'w')
+        fh = open("/home/Cogcc/CogccDatabase/Temporary/cronlog.txt", 'w')
         fh.close()
-    with open("Temporary/cronlog.txt", 'a') as file_handler:
+    with open("/home/Cogcc/CogccDatabase/Temporary/cronlog.txt", 'a') as file_handler:
         current_time = datetime.datetime.now()
-        file_handler.write("Test cron job at: " + str(current_time))
+        file_handler.write("Test cron job at: " + str(current_time) + '\n')
 
 if __name__ == "__main__":
     test()
